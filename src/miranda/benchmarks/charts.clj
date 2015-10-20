3b991fd2d55933c9970faa7f9d285babfb530538
devstopfix <james@devstopfix.net>
2015-10-20 22:29:42 +0100

    Reorder results

--- src/miranda/benchmarks/charts.clj
+++ src/miranda/benchmarks/charts.clj
@@ -1,5 +1,8 @@
 (ns miranda.benchmarks.charts
-  (:use [incanter core charts]))
+  (:use [incanter core charts])
+  (:import (java.awt Color)))
+
+(def miranda-blue (Color/decode "#06b5ba"))
 
 (defn t-to-µs [s]
   (let [conversion {"ms" 1000 "µs" 1}]
@@ -12,16 +15,18 @@
 (def results
   [
    ["array-sort"          (t-to-µs "0.098702 ms")]
+   ["quick-sort"          (t-to-µs "164.110072 µs")]
+   ["quick-sort ordered"  (t-to-µs "934.569847 µs")]
+   ["quick-sort reversed" (t-to-µs "1.080595 ms")]
    ["merge-sort"          (t-to-µs "2.637829 ms")]
    ["merge-sort ordered"  (t-to-µs "2.253188 ms")]
    ["merge-sort reversed" (t-to-µs "2.302531 ms")]
-   ["quick-sort"          (t-to-µs "164.110072 µs")]
-   ["quick-sort ordered"  (t-to-µs "934.569847 µs")]
-   ["quick-sort reversed" (t-to-µs "1.080595 ms")]])
+   ])
+
 
 
 (defn -main []
-  (save
+  (doto
     (bar-chart
       (map first results)
       (map last results)
@@ -29,4 +34,5 @@
       :x-label "Algorithm & dataset"
       :title "Sort Algorithms"
       :vertical false)
-    "docs/sort.png"))
+      (set-stroke-color miranda-blue)
+      (save "docs/sort.png")))
